f10a4d49fdc5dda63e9b8156214901093a28a677
LakshayN <lakshaynarula8888@gmail.com>
2023-03-20 12:42:25 +0530

    first commit

---
+++ src/utils/users.js
@@ -0,0 +1,51 @@
+const users = [];
+
+const addUser = ({ id, username, room }) => {
+  //clean data
+  username = username.trim().toLowerCase();
+  room = room.trim().toLowerCase();
+
+  //validating
+  if (!username || !room) {
+    return {
+      error: "Username and room are required",
+    };
+  }
+
+  //check existing user
+  const existingUser = users.find((user) => {
+    return user.room === room && user.username === username;
+  });
+
+  //validate username
+  if (existingUser) {
+    return {
+      error: "Username is in use",
+    };
+  }
+
+  //store user
+  const user = { id, username, room };
+  users.push(user);
+  return { user };
+};
+
+const removeUser = (id) => {
+  const index = users.findIndex((user) => {
+    return user.id === id;
+  });
+  if (index != -1) {
+    return users.splice(index, 1)[0]; //returning removed user
+  }
+};
+
+const getUser = (id) => {
+  return users.find((user) => user.id === id);
+};
+
+const getUsersInRoom = (room) => {
+  room = room.trim().toLowerCase();
+  return users.filter((user) => user.room === room);
+};
+
+module.exports = { addUser, removeUser, getUser, getUsersInRoom };
